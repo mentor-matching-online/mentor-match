ab3b24c1bef089ed3da9704e5a1d08f4b8459d5b
jonathan <jonodrew@users.noreply.github.com>
2023-05-22 23:03:36 +0100

    Expose the Stack from the Stage

--- mentor-match-infra/mentor_match_infra/mentor_match_stage.py
+++ mentor-match-infra/mentor_match_infra/mentor_match_stage.py
@@ -7,5 +7,8 @@ class MentorMatchAppStage(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        service = MentorMatchStack(self, "MentorMatchStack")
+        self._service = MentorMatchStack(self, "MentorMatchStack")
 
+    @property
+    def web_stack(self) -> MentorMatchStack:
+        return self._service
