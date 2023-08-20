026546244d8577f15bba71d0d18cf3d1f26caf4f
John Peart <4903081+johnpeart@users.noreply.github.com>
2023-08-20 16:19:25 +0100

    Change http:// to https://
    Changed redirects to https:// per [this comment](https://github.com/mentor-matching-online/mentor-match/pull/12#discussion_r1298463557)

--- mentor_match_web/app/main/routes.py
+++ mentor_match_web/app/main/routes.py
@@ -37,12 +37,12 @@ def index():
 
 @main_bp.route("/cookies")
 def cookies():
-    return redirect("http://mentormatching.online/cookies/")
+    return redirect("https://mentormatching.online/cookies/")
 
 
 @main_bp.route("/privacy-and-data")
 def privacy():
-    return redirect("http://mentormatching.online/privacy/")
+    return redirect("https://mentormatching.online/privacy/")
 
 
 @main_bp.route("/upload", methods=["GET", "POST"])
